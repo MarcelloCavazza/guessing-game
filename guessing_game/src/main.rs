6bb6a62704f21662ace60cd948ee1f0a40eb0601
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-12-03 03:31:55 -0300

    fisinhed base game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        //i can just put .to_string() right after $secret_number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small!");
+                println!("Try inserting again.")
+            }
+            Ordering::Greater => {
+                println!("Too big!");
+                println!("Try inserting again.")
+            }
+            Ordering::Equal => {
+                println!("You win!");
+                println!("The sys guessed: {}", secret_number);
+                break;
+            }
+        }
+        println!("You guessed: {}", guess);
+    }
 }
